3a11e65850d175ce4b04561fb2012edb17ac83a6
elena <elena@MacBook-Pro-de-elena.local>
2019-02-24 21:15:48 -0300

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,15 @@ import { NgModule } from '@angular/core';
 //Routes
 import { APP_ROUTES } from './app.routes';
 
+//Modulos
+import { PageModule } from './pages/pages.module';
 
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+
 
 
 
@@ -23,20 +20,15 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    ProgressComponent,
-    BreadcrumsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    PagesComponent,
-    RegisterComponent,
+    RegisterComponent
   ],
+
   imports: [
     BrowserModule,
-    APP_ROUTES 
+    APP_ROUTES,
+    PageModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
